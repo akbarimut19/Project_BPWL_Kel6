476acc8da98bbb30bb913cd09571a3de9a71a5cf
akbarimut19 <rahmatul20ti@mahasiswa.pcr.ac.id>
2021-07-20 01:44:19 +0700

    update v1.1

---
+++ view/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aplikasi Penjualan</title>
+<link href="view/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="view/js/dropdown.js"></script>
+<script type="text/javascript" src="view/js/highslide-with-html.js"></script>
+<script type="text/javascript" src="view/js/slideshow.js"></script>
+<script type="text/javascript" src="view/js/utilities.js"></script>
+</head>
+<body>
+
+<div id="container_wrapper">
+	<div class="spacer"></div>
+<div id="container">	
+
+  <div id="header">
+      <div id="inner_header">
+			<img src='view/images/head.jpg'>
+      </div>
+  </div>
+  
+		<div id="left_column">
+			<div class="text_area" align="justify">	
+			<?php include "kiri.php"; ?>
+			</div>
+		</div>
+		<div style='clear:both;'></div>
+		<div style='color:#fff;' id="footer">
+		<center>Sistem Kasir - Created by: Kel6</center>
+    </div>
+        
+</div>
+<div class="spacer"></div>
+</div>
+</body>
+</html>
